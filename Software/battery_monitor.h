d45f2118ddfdbb3e743170110bc72aa077c8bcaa
Lokendra Singh Shekhawat <skshekhawat89@gmail.com>
2026-02-15 18:25:30 +0530

    Add battery voltage monitoring functionality

--- Software/battery_monitor.h
+++ Software/battery_monitor.h
@@ -1,1 +1,15 @@
+#ifndef BATTERY_MONITOR_H
+#define BATTERY_MONITOR_H
+
+#include <Arduino.h>
+
+#define BATTERY_PIN 34  // ADC pin for voltage sensing
+
+float readBatteryVoltage() {
+  int raw = analogRead(BATTERY_PIN);
+  float voltage = (raw / 4095.0) * 3.3 * 3; // Assuming voltage divider
+  return voltage;
+}
+
+#endif
 
