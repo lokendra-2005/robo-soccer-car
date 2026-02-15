bbec3098c8ab90c38e77591dd1a54ddaf2a6cef1
Lokendra Singh Shekhawat <skshekhawat89@gmail.com>
2026-02-15 18:26:00 +0530

    Add motor control template to main.ino
    Added base motor control code template for ESP32.

--- Software/ESP32/main.ino
+++ Software/ESP32/main.ino
@@ -1,1 +1,32 @@
 
+---
+
+## **3️⃣ software/esp32/main.ino**  
+Base motor control code template:
+
+```cpp
+#include <Arduino.h>
+#include "motor_control.h"
+#include "battery_monitor.h"
+
+// Motor pins
+#define IN1 26
+#define IN2 27
+#define IN3 14
+#define IN4 12
+#define ENA 25
+#define ENB 33
+
+void setup() {
+  Serial.begin(115200);
+  initMotors();
+  Serial.println("Robo Soccer Car Initialized");
+}
+
+void loop() {
+  // Example movement: forward 2 sec, backward 2 sec
+  moveForward(2000);
+  moveBackward(2000);
+  stopMotors(1000);
+}
+
