7182ea37ddfde08d936a424f763908101bebb032
Lokendra Singh Shekhawat <skshekhawat89@gmail.com>
2026-02-15 18:26:28 +0530

    Implement basic motor control functions
    Added motor control functions to manage motor movement.

--- Software/ESP32/motor_control.h
+++ Software/ESP32/motor_control.h
@@ -1,1 +1,52 @@
+#ifndef MOTOR_CONTROL_H
+#define MOTOR_CONTROL_H
+
+#include <Arduino.h>
+
+// Motor pins
+#define IN1 26
+#define IN2 27
+#define IN3 14
+#define IN4 12
+#define ENA 25
+#define ENB 33
+
+void initMotors() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+}
+
+void moveForward(int duration_ms) {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 200);
+  analogWrite(ENB, 200);
+  delay(duration_ms);
+}
+
+void moveBackward(int duration_ms) {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, 200);
+  analogWrite(ENB, 200);
+  delay(duration_ms);
+}
+
+void stopMotors(int duration_ms) {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  delay(duration_ms);
+}
+
+#endif
 
